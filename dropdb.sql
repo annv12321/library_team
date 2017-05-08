cd271a0bab768cc890352c66d2ec9c92d7040a13
flipperswitch <flipperswitch@gmail.com>
2017-05-08 00:54:04 -0400

    Everything tests fine with the gui.  However, there are many things not working with the text imput file.  I suspect the sample data needs to be re-evaluated.

--- dropdb.sql
+++ dropdb.sql
@@ -1,14 +1,13 @@
--drop table category;
--drop table borrower;
--drop table borrower_phone;
--drop table book_info;
--drop table book;
--drop table book_author;
--drop table book_keyword;
--drop table checked_out;
--drop table fine;
--
--drop trigger no_renew_if_overdue;
--drop trigger create_fine;
--drop trigger max_checkedout;
--drop trigger remove_book_info_when_no_copies;
+drop table category;
+drop table borrower;
+drop table borrower_phone;
+drop table book_info;
+drop table book;
+drop table book_author;
+drop table book_keyword;
+drop table checked_out;
+drop table fine;
+
+drop trigger no_renew_if_overdue;
+drop trigger max_checkedout;
+drop trigger remove_book_info_when_no_copies;
